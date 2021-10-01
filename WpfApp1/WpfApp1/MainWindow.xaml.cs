e8f53df241026e045fd174bb2c0df18d58855fc2
PeterMontanna <83061904+PeterMontanna@users.noreply.github.com>
2021-10-01 23:57:21 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+ 
+namespace calculations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        double num1; 
+        double num2; 
+        double totalNumber; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AddingNumbers(object sender, RoutedEventArgs e)
+        {
+            num1 = Convert.ToDouble(numberA.Text); 
+            num2 = Convert.ToDouble(numberB.Text); 
+
+            totalNumber = num1 + num2; 
+
+            result.Content = totalNumber.ToString(); 
+        }
+
+        private void SubtractNumbers(object sender, RoutedEventArgs e)
+        {
+            num1 = Convert.ToDouble(numberA.Text); 
+            num2 = Convert.ToDouble(numberB.Text); 
+
+            totalNumber = num1 - num2; 
+
+            result.Content = totalNumber.ToString(); 
+        }
+
+        private void MultiplyNumbers(object sender, RoutedEventArgs e)
+        {
+            num1 = Convert.ToDouble(numberA.Text); 
+            num2 = Convert.ToDouble(numberB.Text); 
+
+            totalNumber = num1 * num2; 
+
+            result.Content = totalNumber.ToString(); 
+        }
+
+        private void DivideNumbers(object sender, RoutedEventArgs e)
+        {
+            num1 = Convert.ToDouble(numberA.Text); 
+            num2 = Convert.ToDouble(numberB.Text); 
+
+            totalNumber = num1 / num2; 
+
+            totalNumber = Math.Round(totalNumber, 2); 
+
+            result.Content = totalNumber.ToString(); 
+        }
+    }
+}
